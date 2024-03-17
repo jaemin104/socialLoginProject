513a7067af7bac78e660250966cd2c2ea890632e
jaemin <102853672+jaemin104@users.noreply.github.com>
2024-03-17 22:30:48 +0900

    SQL) create table
    테이블 생성 오류 수정 중

--- sql/create.sql
+++ sql/create.sql
@@ -1,1 +1,32 @@
+CREATE DATABASE users_DB;
 
+CREATE TABLE users (
+  userId VARCHAR(60) PRIMARY KEY,
+  userName VARCHAR(20),
+  profileImg VARCHAR(200),
+  created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  Updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE
+);
+
+INSERT INTO users(userId, userName, profileImg) VALUES
+('jaemin104', '김재민', 'image1'),
+('jaemin105', '김재맨', 'image2'),
+('asd', '누구', 'image3'),
+('rootpark', '박근원', 'image4'),
+('gijae', '김기재', 'image5');
+
+SELECT userId, userName
+FROM users
+ORDER BY created DESC
+LIMIT 1;
+
+SELECT userName
+FROM users
+WHERE userId = 'asd';
+
+DELETE FROM users
+WHERE userName = '박근원';
+
+UPDATE users
+SET userId = 'dsa'
+WHERE userName = '김기재';
