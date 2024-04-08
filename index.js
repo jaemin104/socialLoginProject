dd2f78d66a70700730b0942e3b62400393f3d757
jaemin104 <kjm041107@naver.com>
2024-04-08 18:56:08 +0900

    Test

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import cors from 'cors';
 
+import { ping } from './controller/system.js';
+
 const serverPort = 3000;
 const app = express();
 app.use(bodyParser.json());
@@ -9,14 +11,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 const router = express.Router();
 
-const ping = (req, res) => {
-    const result = true;
-    res.send({result});
-};
-
 router.route('/ping').get(ping);
 
 app.use('/socialLoginProject/api/v1',router);
 app.listen(serverPort);
-
-//http://51.20.71.68:3000/socialLoginProject/api/v1/ping
+//http://3.27.152.254:3000/socialLoginProject/api/v1/ping
